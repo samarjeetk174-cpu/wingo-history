cedf1ac58cbccf53fa036dc0c38b8b5dbe64b5d3
samarjeetk174-cpu <samarjeetk174@gmail.com>
2026-09-24 15:40:27 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -20,7 +20,7 @@ else:
 
 def fetch_and_sync():
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"
     }
     
     combined = []
