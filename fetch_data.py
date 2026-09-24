b9956e5e8cf6641a6153630be7b58e8fbcb84ab8
samarjeetk174-cpu <samarjeetk174@gmail.com>
2026-09-24 16:02:50 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -27,7 +27,7 @@ def fetch_and_sync():
     # 500 रिकॉर्ड्स के लिए 10 पेज तक लूप
     for page in range(1, 11):
         url = f"https://draw.ar-lottery01.com/WinGo/WinGo_1M/GetHistoryIssuePage.json?pageNo={page}&pageSize=50"
-                try:
+        try:
             r = requests.get(url, headers=headers, timeout=10)
             data = r.json()
             items = data.get("data", {}).get("list", [])
